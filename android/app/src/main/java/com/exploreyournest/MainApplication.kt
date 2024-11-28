aa809d322997f084121a73f19ad237a6ef05646f
Mahammad Murshid <98098347+murshidmurshi@users.noreply.github.com>
2024-11-28 06:43:57 +0530

    28thpush

--- android/app/src/main/java/com/exploreyournest/MainApplication.kt
+++ android/app/src/main/java/com/exploreyournest/MainApplication.kt
@@ -3,7 +3,6 @@ package com.exploreyournest
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
@@ -11,16 +10,15 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.oney.WebRTCModule.WebRTCModuleOptions // Import WebRTC for screen sharing
 
 class MainApplication : Application(), ReactApplication {
 
+  // Set up React Native
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
+            PackageList(this).packages
 
         override fun getJSMainModuleName(): String = "index"
 
@@ -33,12 +31,17 @@ class MainApplication : Application(), ReactApplication {
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
+  // Called when the app starts
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
+
+    // Enable screen sharing feature
+    val options = WebRTCModuleOptions.getInstance()
+    options.enableMediaProjectionService = true // Turn on screen sharing service
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+      load() // Load New Architecture if enabled
     }
   }
 }
